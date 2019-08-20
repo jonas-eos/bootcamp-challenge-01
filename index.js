4f256677c68edaa299b8a702ff7de1264a3f9d03
Jonas Evangelista <jonas.eos@outlook.com>
2019-08-20 20:25:44 -0300

    feature: Projects route
    List all saved projects

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-const express = require("Express");
+/**
+ * Dependencies
+ */
+const express = require("express");
 
+/**
+ * Server settings
+ */
 const server = express();
+server.use(express.json());
 
+/**
+ * Consts
+ */
+const projects = {};
+
+/**
+ * Core system
+ */
+
+/**
+ * Router /projects
+ * Lista all projects saved
+ * @param __request
+ * @param __response
+ *
+ * @return projects array
+ */
+server.get("/projects", (__request, __response) => {
+  return __response.json(projects);
+});
+
+/**
+ * Server listen
+ */
 server.listen(8080);
