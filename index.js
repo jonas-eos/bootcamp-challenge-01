ce734bc19a7655afa1c5c0d7c3cc9f40634ca383
Jonas Evangelista <jonas.eos@outlook.com>
2019-08-20 22:36:26 -0300

    feature: Middleware
    * Middleware to log control with request ID
    * Middleware to check if the project (by ID) exists on const base.

--- index.js
+++ index.js
@@ -4,15 +4,57 @@
 const express = require("express");
 
 /**
+ * Consts
+ */
+const projects = [];
+
+// Counter
+let requestCount = 0;
+
+/**
  * Server settings
  */
 const server = express();
 server.use(express.json());
 
 /**
- * Consts
+ * Middlewares
  */
-const projects = [];
+
+/**
+ * Middleware
+ * Print log with request ID
+ * @param __request
+ * @param __response
+ * @param __next
+ *
+ * @return console with request ID.
+ */
+server.use((__request, __response, __next) => {
+  requestCount++;
+  console.log(`Request ID: ${requestCount}`);
+  return __next();
+});
+
+/**
+ * Middleware
+ * Check if exist a project by ID params
+ * @param __request
+ * @param __response
+ * @param __next
+ *
+ * @return next call
+ */
+function checkProjectExists(__request, __response, __next) {
+  const { id } = __request.params;
+  const project = projects.find(index => index.id == id);
+  if (!project) {
+    return __response.status(400).json({
+      error: "Project does not exists!"
+    });
+  }
+  return __next();
+}
 
 /**
  * Core system
@@ -57,13 +99,17 @@ server.post("/projects", (__request, __response) => {
  *
  * @return projects updated
  */
-server.post("/projects/:id/tasks", (__request, __response) => {
-  const { id } = __request.params;
-  const { title } = __request.body;
-  const project = projects.find(index => index.id == id);
-  project.tasks.push(title);
-  return __response.json(projects);
-});
+server.post(
+  "/projects/:id/tasks",
+  checkProjectExists,
+  (__request, __response) => {
+    const { id } = __request.params;
+    const { title } = __request.body;
+    const project = projects.find(index => index.id == id);
+    project.tasks.push(title);
+    return __response.json(projects);
+  }
+);
 
 /**
  * Router /projects/:id
@@ -73,7 +119,7 @@ server.post("/projects/:id/tasks", (__request, __response) => {
  *
  * @return projects updated
  */
-server.put("/projects/:id", (__request, __response) => {
+server.put("/projects/:id", checkProjectExists, (__request, __response) => {
   const { id } = __request.params;
   const { title } = __request.body;
   const project = projects.find(index => index.id == id);
@@ -89,7 +135,7 @@ server.put("/projects/:id", (__request, __response) => {
  *
  * @return projects updated
  */
-server.delete("/projects/:id", (__request, __response) => {
+server.delete("/projects/:id", checkProjectExists, (__request, __response) => {
   const { id } = __request.params;
   const projectIndex = projects.findIndex(index => index.id == id);
   projects.splice(projectIndex, 1);
